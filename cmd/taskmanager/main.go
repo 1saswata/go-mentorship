fe91839472a2bb3c2b9f129ea38391f38a14ad42
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2026-02-09 18:00:04 +0530

    feat: Implement create and read task
    Get and Post handlers for /tasks to store and
    display tasks in json format depending on http
    request type

--- cmd/taskmanager/main.go
+++ cmd/taskmanager/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
@@ -9,8 +10,33 @@ import (
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "OK\n")
 }
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var t Task
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(t)
+}
+
+func ListTaskHandler(w http.ResponseWriter, r *http.Request) {
+	tasks := []Task{
+		{1, "Feed the cat", "Complete"},
+		{2, "Pet the cat", "Ongoing"},
+		{5, "Let the cat sleep", "Incomplete"},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", HealthCheckHandler)
+	mux.HandleFunc("GET /tasks", ListTaskHandler)
+	mux.HandleFunc("POST /tasks", CreateTaskHandler)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
