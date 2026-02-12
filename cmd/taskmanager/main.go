a68e6008c3b81863dba10def2c9037f1e789037e
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2026-02-12 17:15:14 +0530

    feat: Memory based CRUD operations for /tasks
    Implemented TaskStore and its functions to store
    and read tasks into/from memory

--- cmd/taskmanager/main.go
+++ cmd/taskmanager/main.go
@@ -7,36 +7,39 @@ import (
 	"net/http"
 )
 
+type taskServer struct {
+	store *TaskStore
+}
+
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "OK\n")
 }
 
-func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+func (ts *taskServer) CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
 	var t Task
 	err := json.NewDecoder(r.Body).Decode(&t)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	id := ts.store.CreateTask(t.Name, t.Status)
+	t.ID = id
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(t)
 }
 
-func ListTaskHandler(w http.ResponseWriter, r *http.Request) {
-	tasks := []Task{
-		{1, "Feed the cat", "Complete"},
-		{2, "Pet the cat", "Ongoing"},
-		{5, "Let the cat sleep", "Incomplete"},
-	}
+func (ts *taskServer) ListTaskHandler(w http.ResponseWriter, r *http.Request) {
+	tasks := ts.store.GetAllTasks()
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(tasks)
 }
 
 func main() {
 	mux := http.NewServeMux()
+	tasks := taskServer{store: NewTaskStore()}
 	mux.HandleFunc("/health", HealthCheckHandler)
-	mux.HandleFunc("GET /tasks", ListTaskHandler)
-	mux.HandleFunc("POST /tasks", CreateTaskHandler)
+	mux.HandleFunc("GET /tasks", tasks.ListTaskHandler)
+	mux.HandleFunc("POST /tasks", tasks.CreateTaskHandler)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
--- cmd/taskmanager/store.go
+++ cmd/taskmanager/store.go
@@ -1,7 +1,45 @@
 package main
 
+import "sync"
+
 type Task struct {
 	ID     int    `json:"id"`
 	Name   string `json:"name"`
 	Status string `json:"status"`
+}
+
+type TaskStore struct {
+	sync.Mutex
+	tasks  map[int]Task
+	nextID int
+}
+
+func NewTaskStore() *TaskStore {
+	return &TaskStore{
+		tasks:  make(map[int]Task),
+		nextID: 1,
+	}
+}
+
+func (t *TaskStore) CreateTask(name, status string) int {
+	t.Lock()
+	defer t.Unlock()
+	task := Task{
+		ID:     t.nextID,
+		Name:   name,
+		Status: status,
+	}
+	t.tasks[t.nextID] = task
+	t.nextID++
+	return task.ID
+}
+
+func (t *TaskStore) GetAllTasks() []Task {
+	t.Lock()
+	defer t.Unlock()
+	allTasks := make([]Task, 0, len(t.tasks))
+	for _, task := range t.tasks {
+		allTasks = append(allTasks, task)
+	}
+	return allTasks
 }
