e1166322ceab41743c8af97eecaac21ae45c8172
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2026-02-18 10:56:24 +0530

    feat: Implement timeout context for checkUrl

--- cmd/site-checker/main.go
+++ cmd/site-checker/main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 )
 
-func checkUrl(url string, wg *sync.WaitGroup, results chan string) {
+func checkUrl(ctx context.Context, url string, wg *sync.WaitGroup, results chan<- string) {
 	defer wg.Done()
-	resp, err := http.Get(url)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
-		results <- fmt.Sprintf("%s - DOWN (Network Error)", url)
+		results <- fmt.Sprintf("%s - DOWN (Bad Request) %d", url, http.StatusBadRequest)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	select {
+	case <-ctx.Done():
+		results <- fmt.Sprintf("%s - DOWN (Status: %d)", url, http.StatusRequestTimeout)
 		return
+	default:
+		if err != nil {
+			results <- fmt.Sprintf("%s - DOWN (Network Error)", url)
+			return
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode == http.StatusOK {
+			results <- fmt.Sprintf("%s - UP", url)
+		} else {
+			results <- fmt.Sprintf("%s - DOWN (Status: %d)", url, resp.StatusCode)
+		}
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK {
-		results <- fmt.Sprintf("%s - UP", url)
-	} else {
-		results <- fmt.Sprintf("%s - DOWN (Status: %d)", url, resp.StatusCode)
-	}
+
 }
 
 func main() {
@@ -29,10 +42,13 @@ func main() {
 		"http://doesntexist.com",
 		"dadsadadw.dawds"}
 	var wg sync.WaitGroup
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 900*time.Millisecond)
+	defer cancel()
 	results := make(chan string)
 	for _, url := range urls {
 		wg.Add(1)
-		go checkUrl(url, &wg, results)
+		go checkUrl(ctx, url, &wg, results)
 	}
 	go func() {
 		wg.Wait()
