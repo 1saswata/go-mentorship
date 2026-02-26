410a759317427a53ca1a640b1ddba5dcad9c08b5
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2026-02-26 15:40:29 +0530

    ref: Restructure taskmanager to implement internal

--- cmd/taskmanager/main.go
+++ cmd/taskmanager/main.go
@@ -2,99 +2,25 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"encoding/json"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 	"time"
+
+	"github.com/1saswata/go-mentorship/internal/handlers"
+	"github.com/1saswata/go-mentorship/internal/middleware"
+	"github.com/1saswata/go-mentorship/internal/store"
 )
-
-type taskServer struct {
-	store *TaskStore
-}
-
-func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "OK\n")
-}
-
-func (ts *taskServer) CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
-	var t Task
-	err := json.NewDecoder(r.Body).Decode(&t)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	id := ts.store.CreateTask(t.Name, t.Status)
-	t.ID = id
-	w.Header().Set("Content-Type", "application/json")
-	if id == -1 {
-		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(t)
-	}
-}
-
-func (ts *taskServer) ListTaskHandler(w http.ResponseWriter, r *http.Request) {
-	tasks := ts.store.GetAllTasks()
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
-}
-
-func (ts *taskServer) UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
-	var t Task
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	err = json.NewDecoder(r.Body).Decode(&t)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	err = ts.store.UpdateTaskStatus(id, t.Status)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusNoContent)
-}
-
-func (ts *taskServer) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	err = ts.store.DeleteTask(id)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusNoContent)
-}
 
 func main() {
 	db := InitDB()
 	defer db.Close()
 	mux := http.NewServeMux()
-	wrappedMux := LoggingMiddleware(mux)
-	tasks := taskServer{store: NewTaskStore(db)}
-	mux.HandleFunc("/health", HealthCheckHandler)
+	wrappedMux := middleware.LoggingMiddleware(mux)
+	tasks := handlers.TaskServer{Store: store.NewTaskStore(db)}
+	mux.HandleFunc("/health", handlers.HealthCheckHandler)
 	mux.HandleFunc("GET /tasks", tasks.ListTaskHandler)
 	mux.HandleFunc("POST /tasks", tasks.CreateTaskHandler)
 	mux.HandleFunc("PUT /tasks/{id}", tasks.UpdateTaskHandler)
